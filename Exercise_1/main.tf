cfd564c1b612dfd8136106a392b43e2c022096ea
abondar24 <abondar1992@gmail.com>
2020-06-07 23:55:42 +0200

    task 5-1

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,39 @@
 # TODO: Designate a cloud provider, region, and credentials
 
+provider "aws" {
+  access_key = "AKIAS6UDOULBXN7JAQ6E"
+  secret_key = "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"
+  region = "eu-west-1"
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 
+variable "t2_count" {
+  default = "4"
+}
+
+resource "aws_instance" "Udacity_T2" {
+  ami = "ami-0ea3405d2d2522162"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-e69cf3bc"
+  count = "${var.t2_count}"
+  tags = {
+   Name = "MICRO-${count.index + 1}"
+  }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+
+variable "m4_count" {
+  default = "2"
+}
+
+resource "aws_instance" "Udacity_M4" {
+  ami = "ami-0ea3405d2d2522162"
+  instance_type = "m4.large"
+  subnet_id = "subnet-e69cf3bc"
+  count = "${var.m4_count}"
+  tags = {
+   Name = "LARGE-${count.index + 1}"
+  }
+}
